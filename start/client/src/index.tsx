9a54cf46abf36879938c9b09bc1ad4565a8244b9
Aleksandr Kolesnikov <bobulbulbul@gmail.com>
2020-05-14 21:22:35 +0300

    Manage local state

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -6,22 +6,27 @@ import React from 'react';
 import ReactDOM from 'react-dom'; 
 import Pages from './pages';
 import injectStyles from './styles';
+import { resolvers, typeDefs } from './resolvers';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
   uri: 'http://localhost:4000/'
 });
 
+
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   link: new HttpLink({
     uri: 'http://localhost:4000/graphql',
-
     headers: {
       authorization: localStorage.getItem('token'),
-    }, 
+    },
   }),
+
+  typeDefs,
+  resolvers,
 });
+
 
 cache.writeData({
   data: {
